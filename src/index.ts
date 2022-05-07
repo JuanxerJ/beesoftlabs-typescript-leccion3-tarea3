b453a0a10e4c6f46958754f59159beb972d6c76a
JuanxerJ <jjhenao.jar@gmail.com>
2022-05-06 22:26:28 -0500

    Resolve third exercise

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,17 @@ myPromise2.then((value: number) => {
   console.log(value + 1);
 });
 //------------------------------------- Ejercicio 3 --------------------------------------------------------//
+const myPromise3: Promise<number> = new Promise((resolve) => {
+  resolve(1);
+});
 
+myPromise3
+  .then((value) => {
+    return value + 2;
+  })
+  .then((value) => {
+    console.log(value + 1); // log: 4
+  });
 //------------------------------------- Ejercicio 4 --------------------------------------------------------//
 
 //------------------------------------- Ejercicio 5 --------------------------------------------------------//
